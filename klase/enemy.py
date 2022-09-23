55d1ede35bbdb16f7c06f9c713f9c0132f912b39
klucic5 <48219043+klucic5@users.noreply.github.com>
2022-09-23 16:51:14 +0200

    Update and rename razredi.py to enemy.py

---
+++ klase/enemy.py
@@ -0,0 +1,10 @@
+class Plovilo:
+    def __init__(self, x,y,velicina, status):
+        '''klasa Brod...'''
+        pass
+
+
+class Brod(Plovilo):
+    def __init__(self, x,y,velicina, status,tip):
+        '''nasljeđuje klasu Brod...'''
+        pass

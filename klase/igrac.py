4297fb3cbd87a42512f355237ea22ffd067282af
klucic5 <48219043+klucic5@users.noreply.github.com>
2022-09-23 16:47:52 +0200

    Update and rename nastavnici.py to igrac.py

---
+++ klase/igrac.py
@@ -0,0 +1,17 @@
+class Igrac:
+    def __init__(self, ime, email, bodovi):
+        '''klasa Igrac...'''
+        pass
+
+
+class Maloljetan(Igrac):
+    def __init__(self, ime, email, bodovi, god):
+        '''nasljeđuje klasu Igrac...'''
+        pass
+
+
+class Profi(Igrac):
+    def __init__(self, ime, email, bodovi, rang):
+        '''nasljeđuje klasu Igrac... '''
+        pass
+
